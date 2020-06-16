389af5fce23a3fa5c59f8bcb56a579622742c54a
PES1201800182 <56343587+PES1201800182@users.noreply.github.com>
2020-06-16 20:03:16 +0530

    Update nextPowerOf2.py

--- basic_datastructures/bits/nextPowerOf2.py
+++ basic_datastructures/bits/nextPowerOf2.py
@@ -1,1 +1,9 @@
-
+def nextPowerOf2(value):
+    if(value==0):
+        return 1
+    elif(value&(value-1)==0):
+        return value
+    else:
+        while(value&(value-1)!=0):
+            value=value&(value-1)
+        return value<<1
